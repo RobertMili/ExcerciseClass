00c8f4bc1b59d1b5e2f38535f6e92e87e732cb80
RobertMili <robertmilicevic3869@gmail.com>
2022-09-21 20:16:51 +0200

    asdsa

--- src/Main/Dealer.java
+++ src/Main/Dealer.java
@@ -39,7 +39,7 @@ public class Dealer extends Person {
                 ", moneyBank=" + moneyBank +
                 '}';
     }
-    // Här Martin :
+
     public void selling(SuperClass vehicle, Selling selling, Buyer buyer, Dealer dealer){
         System.out.println("\nOwner of vehicle it is " + vehicle.getOwner());
         System.out.println("\nBuyer is: " + buyer.getName());
@@ -62,7 +62,7 @@ public class Dealer extends Person {
         dealer.setMoneyBank(vehicle.getPrice() * 0.2);
 
     }
-    // Och den här också Martinb
+    // Och den här också Martin
     public void sellingCar(Car vehicle, Selling selling, Buyer buyer, Dealer dealer){
         System.out.println("\nOwner of vehicle it is " + vehicle.getOwner());
         System.out.println("\nBuyer is: " + buyer.getName());
@@ -84,4 +84,27 @@ public class Dealer extends Person {
         buyer.setMoneyBank(vehicle.getPrice() - buyer.getMoneyBank());
         dealer.setMoneyBank(vehicle.getPrice() * 0.2);
     }
+    // Och den här också Martin
+    public void sellingCar(MotorCycle vehicle, Selling selling, Buyer buyer, Dealer dealer){
+        System.out.println("\nOwner of vehicle it is " + vehicle.getOwner());
+        System.out.println("\nBuyer is: " + buyer.getName());
+        System.out.println("\nVehicle is: " + vehicle.getClass());
+        vehicle.setOwner(buyer.getName());
+
+        transferCash(vehicle, selling,buyer,dealer);
+
+        System.out.println("\nNew owner of " + vehicle + " is " + vehicle.getOwner());
+        System.out.println("\nSelling  are " + selling.getName());
+        System.out.println("\nBuyer is " + buyer.getName());
+        System.out.println("\nBuyer have now $" + buyer.getMoneyBank());
+        System.out.println("\nSeller have  $ " + selling.getMoneyBank());
+        System.out.println("\nDealer have $ " + dealer.getMoneyBank());
+    }
+
+    private static void transferCashCar(MotorCycle vehicle,Selling selling, Buyer buyer, Dealer dealer) {
+        selling.setMoneyBank(selling.getMoneyBank() + vehicle.getPrice());
+        buyer.setMoneyBank(vehicle.getPrice() - buyer.getMoneyBank());
+        dealer.setMoneyBank(vehicle.getPrice() * 0.2);
+    }
+
 }
